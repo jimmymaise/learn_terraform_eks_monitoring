90d8816141862cf4d205a1022f5f90c05233c287
jimmymaise <the@duyet.com>
2022-03-24 23:29:49 -0700

    add service_account_name

--- eks-with-monitoring/service_account.tf
+++ eks-with-monitoring/service_account.tf
@@ -1,10 +1,23 @@
+#Test command
+# kubectl auth can-i list pods --as=system:serviceaccount:my-name-space:sa-list-pods -n my-name-space
+
 locals {
-  name_space = "my_name_space"
+  name_space           = "my-name-space"
+  service_account_name = "sa-list-pods"
+}
+
+resource "kubernetes_namespace" "this" {
+  metadata {
+    labels = {
+    }
+
+    name = local.name_space
+  }
 }
 
 resource "kubernetes_service_account" "get_list_pods" {
   metadata {
-    name      = "sa-list-pods"
+    name      = local.service_account_name
     namespace = local.name_space
     #    annotations = {
     #      "eks.amazonaws.com/role-arn" = var.role_arn
@@ -17,9 +30,9 @@ resource "kubernetes_service_account" "get_list_pods" {
 
 resource "kubernetes_role" "get_list_pods" {
   metadata {
-    name   = "get-list-pods"
-    namespace : local.name_space
-    labels = {
+    name      = "get-list-pods"
+    namespace = local.name_space
+    labels    = {
       test = "MyRole"
     }
   }
@@ -39,11 +52,11 @@ resource "kubernetes_role_binding" "get_list_pods" {
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "Role"
-    name      = kubernetes_role.get_list_pods.metadata.name
+    name      = kubernetes_role.get_list_pods.metadata[0].name
   }
   subject {
     kind      = "ServiceAccount"
-    name      = kubernetes_service_account.get_list_pods.metadata.name
+    name      = kubernetes_service_account.get_list_pods.metadata[0].name
     namespace = local.name_space
   }
 }
